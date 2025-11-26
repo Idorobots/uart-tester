18657d45d84e73cb082aac5426baeb01cabdbb8e
Kajetan Rzepecki <k@spartan.works>
2025-11-26 14:00:39 +0100

    Refactored the SRAM test script.

--- scripts/sram.py
+++ scripts/sram.py
@@ -16,12 +16,12 @@ tester = None
 
 failures = 0
 
-def test(actual, expected, info):
+def test(actual, expected, addr, info):
     global failures
 
     if actual != expected:
         failures = failures + 1
-        print("Test failed: {}, {} != {}".format(info, expected, actual))
+        print("Test failed: {}, {} != {} at address {:04x}".format(info, expected, actual, addr))
 
 def send(command):
     tester.write(command + b"\n")
@@ -55,73 +55,77 @@ def read_inputs():
         print(value)
     return value
 
-# 32k SRAM:
-# Data: I0-I8, O0-O8 via 100k
-# Address: O8-O22
-# /CS: O29
+# 512k SRAM (32 pin DIP):
+# Data: I0-I7, O0-O7 via latch
+# Address: O8-O26
+# CS2: ---
+# /CS1: O29
 # /OE: O30
-# /WE: 031
+# /WE: O31
 
-def check_32k_sram(we, oe, cs, addr, data_str):
-    addr_str = bytes("{0:016b}".format(addr & 0xffff)[-15:], 'latin1')
-    set_outputs((we and b"0" or b"1") + (oe and b"0" or b"1") + (cs and b"0" or b"1") + b"000000" + addr_str + data_str)
-    return read_inputs()
+# 256k SRAM (32 pin DIP):
+# Data: I0-I7, O0-O7 via latch
+# Address: O8-O25
+# CS2: O26
+# /CS1: O29
+# /OE: O30
+# /WE: O31
 
-def test_32k_sram():
-    test(check_32k_sram(False, False, False, 0x0000, ZERO), HIGHZ, "Outputs floating when no CS")
+# 128k SRAM (32 pin DIP):
+# Data: I0-I7, O0-O7 via latch
+# Address: O8-O24
+# CS2: O26
+# /CS1: O29
+# /OE: O30
+# /WE: O31
 
-    addr_max = 2**15
+# 32k SRAM (28 pin DIP):
+# Data: I0-I7, O0-O7 via latch
+# Address: O8-O22
+# CS2: ---
+# /CS1: O29
+# /OE: O30
+# /WE: O31
 
-    for i, pattern in enumerate(PATTERNS):
-        b = (1 << (i + 1)) - 1
-        toggle = True
-        set_bar(b)
-
-        print("Pattern: ", pattern)
-
-        for addr in range(addr_max):
-            if(addr % 128 == 0):
-                toggle = not toggle
-                set_bar(toggle and (b | (1 << i)) or (b & ~(1 << i)))
-
-            # Write data
-            test(check_32k_sram(True, False, True, addr, pattern), pattern, "Writing data works")
-            # Turn off
-            test(check_32k_sram(False, False, False, addr, ZERO), HIGHZ, "Floating after write")
-            # Read data back
-            test(check_32k_sram(False, True, True, addr, ZERO), pattern, "Reading data works")
-
-        if RETENTION:
-            for addr in range(addr_max):
-                # Read data back
-                test(check_32k_sram(False, True, True, addr, ZERO), pattern, "Data retained")
-                # Turn off
-                test(check_32k_sram(False, False, False, addr, ZERO), HIGHZ, "Floating after read")
-
-# 8k SRAM:
-# Data: I0-I8, O0 - O8 via 100k
+# 8k SRAM (28 pin DIP):
+# Data: I0-I7, O0 - O7 via latch
 # Address: O8-O20
 # CS2: O21
 # /CS1: O29
 # /OE: O30
-# /WE: 031
+# /WE: O31
 
-def check_8k_sram(we, oe, cs1, cs2, addr, data_str):
-    addr_str = bytes("{0:016b}".format(addr & 0xffff)[-13:], 'latin1')
-    set_outputs((we and b"0" or b"1") + (oe and b"0" or b"1") + (cs1 and b"0" or b"1") + b"0000000" + (cs2 and b"1" or b"0") + addr_str + data_str)
+def check_sram(cs2_pin, we, oe, cs1, cs2, addr, data_str):
+    value = 0
+    value = value | (addr << 8)
+    if not we:
+        value = value | (1 << 31)
+    if not oe:
+        value = value | (1 << 30)
+    if not cs1:
+        value = value | (1 << 29)
+    if cs2_pin != None and cs2:
+        value = value | (1 << cs2_pin)
+
+    cmd = bytes("{0:032b}".format(value & 0xffffffff), 'latin1')[0:-8] + data_str
+
+    set_outputs(cmd)
     return read_inputs()
 
-def test_8k_sram():
-    test(check_8k_sram(False, False, False, False, 0x0000, ZERO), HIGHZ, "Outputs floating when no CS")
-    test(check_8k_sram(False, False, False, True, 0x0000, ZERO), HIGHZ, "Outputs floating when no CS1")
-    test(check_8k_sram(False, False, True, False, 0x0000, ZERO), HIGHZ, "Outputs floating when no CS2")
+def test_sram(cs2_pin, addr_lines):
+    test(check_sram(cs2_pin, False, False, False, True, 0x0000, ZERO), HIGHZ, None, "Outputs floating when no CS1")
 
-    addr_max = 2**13
+    if cs2_pin != None:
+        test(check_sram(cs2_pin, False, False, False, False, 0x0000, ZERO), HIGHZ, None, "Outputs floating when no CS")
+        test(check_sram(cs2_pin, False, False, True, False, 0x0000, ZERO), HIGHZ, None, "Outputs floating when no CS2")
+
+    addr_max = 2**addr_lines
 
     for i, pattern in enumerate(PATTERNS):
         b = (1 << (i + 1)) - 1
         toggle = True
         set_bar(b)
+
         print("Pattern: ", pattern)
 
         for addr in range(addr_max):
@@ -130,17 +134,22 @@ def test_8k_sram():
                 set_bar(toggle and (b | (1 << i)) or (b & ~(1 << i)))
 
             # Write data
-            test(check_8k_sram(True, False, True, True, addr, pattern), pattern, "Writing data works")
+            test(check_sram(cs2_pin, True, False, True, True, addr, pattern), pattern, addr, "Writing data works")
             # Turn off
-            test(check_8k_sram(False, False, False, False, addr, ZERO), HIGHZ, "Floating after write")
+            test(check_sram(cs2_pin, False, False, False, False, addr, ZERO), HIGHZ, addr, "Floating after write")
             # Read data back
-            test(check_8k_sram(False, True, True, True, addr, ZERO), pattern, "Reading data works")
+            test(check_sram(cs2_pin, False, True, True, True, addr, ZERO), pattern, addr, "Reading data works")
+
         if RETENTION:
             for addr in range(addr_max):
+                if(addr % 128 == 0):
+                    toggle = not toggle
+                    set_bar(toggle and (b | (1 << i)) or (b & ~(1 << i)))
                 # Read data back
-                test(check_8k_sram(False, True, True, True, addr, ZERO), pattern, "Data retained")
+                test(check_sram(cs2_pin, False, True, True, True, addr, ZERO), pattern, addr, "Data retained")
                 # Turn off
-                test(check_8k_sram(False, False, False, False, addr, ZERO), HIGHZ, "Floating after read")
+                test(check_sram(cs2_pin, False, False, False, False, addr, ZERO), HIGHZ, addr, "Floating after read")
+    pass
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -148,7 +157,7 @@ if __name__ == "__main__":
                     description='Tests 8k, 32k and 512k SRAM chips',
                     epilog='')
     parser.add_argument('--port', type=str, default="/dev/ttyACM0")
-    parser.add_argument('-s', '--size', type=int, default=32)
+    parser.add_argument('-s', '--size', type=int, choices=[8, 32, 128, 256, 512])
     parser.add_argument('-r', '--retention', action='store_true', default=False)
     parser.add_argument('--debug', action='store_true', default=False)
 
@@ -161,16 +170,19 @@ if __name__ == "__main__":
     with ser as s:
         tester = s
         try:
+            print("Testing {}k SRAM".format(args.size))
             reset()
 
             if args.size == 512:
-                 pass
+                 test_sram(None, 19)
+            elif args.size == 256:
+                 test_sram(26, 18)
+            elif args.size == 128:
+                 test_sram(26, 17)
             elif args.size == 32:
-                 print("Testing 32k SRAM")
-                 test_32k_sram()
+                 test_sram(None, 15)
             elif args.size == 8:
-                 print("Testing 8k SRAM")
-                 test_8k_sram()
+                 test_sram(21, 13)
             else:
                  print("Unsuported RAM size specified.")
 
